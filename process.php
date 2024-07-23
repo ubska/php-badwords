4a55bab12b75861754fe1c61531d1efc8e1f65e4
Oleksandra Ubska <alessandraubska@gmail.com>
2024-07-23 16:25:08 +0200

    feat: add process.php

---
+++ process.php
@@ -0,0 +1,22 @@
+<?php
+
+$paragraph = $_POST['paragraph'];
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p> <?php echo $paragrafo ?></p>
+</body>
+</html>
+
+
